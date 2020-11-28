a79662f8cf575236dbb969e9c958f3dae6d914e7
Gubin <gub-dmit@bk.ru>
2020-11-28 13:09:11 +0700

    deleted useless files

--- src/botConfig.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.channelId = exports.prefix = exports.token = void 0;
-exports.token = 'NzUxMDM5NDg2ODM3OTE1NzE5.X1DSBw.tFG63vgi6EE5ff79IVhRbqx4bXo';
-exports.prefix = '!';
-exports.channelId = '';
--- src/botConfig.ts
+++
@@ -1,3 +0,0 @@
-export const token: string = 'NzUxMDM5NDg2ODM3OTE1NzE5.X1DSBw.tFG63vgi6EE5ff79IVhRbqx4bXo';
-export const prefix: string = '!';
-export const channelId: string = '';
